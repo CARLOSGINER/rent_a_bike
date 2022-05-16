066978658106addec4f51483cbef180707d0503b
Carlos Giner <cginer@orienteed.com>
2022-05-16 09:36:02 +0200

    Refactor | Select input component to take defaultption prop

--- src/components/SelectInput/index.jsx
+++ src/components/SelectInput/index.jsx
@@ -1,12 +1,11 @@
 import { Form } from "react-bootstrap"
 import { FilterData } from "../../utils/filterData";
-import "./selectInput.css";
 
-export default function SelectInput({ handleChange, data }) {
+export default function SelectInput({ handleChange, data, className, defaultOption="" }) {
   return (
     <>
-      <Form.Select className="input_select" onChange={(e) => handleChange(e)}>
-        <option>See All</option>
+      <Form.Select className={className} onChange={(e) => handleChange(e)}>
+        <option>{defaultOption}</option>
         {FilterData.oneOfEachKind(data, "type").map((type, index) => (
           <option key={index}>{type}</option>
         ))}
