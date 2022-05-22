1aa04d332a9a310bb49ed7b73a788ae44694a055
Carlos Giner <carlosginer.webdev@gmail.com>
2022-05-22 09:43:32 +0200

    Fix | Center home text for mobile

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,13 @@
 import { useTranslation } from 'react-i18next'
+import './home.css';
 
 export default function Home() {
 
   const [t] = useTranslation("global");
 
   return (
-    <div>{t("home.greetings")}</div>
+    <div className="home-container">
+      <p>{t("home.greetings")}</p>
+    </div>
   )
 }
